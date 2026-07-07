e244e1b1d088a4e627007b669277a58caa2f81b0
asm-saim <saim777m@gmail.com>
2026-07-07 09:11:38 +0600

    task section responsiveness completed

--- src/components/AllTickets/AllTickets.jsx
+++ src/components/AllTickets/AllTickets.jsx
@@ -2,31 +2,41 @@ import { use } from "react";
 import Ticket from "../Ticket/Ticket";
 import TasksSection from "../tasksSection/TasksSection";
 
-const AllTickets = ({ fetchData, customerTicket, setCustomerTicket,
-    handleResolved, resolvedTicket }) => {
+const AllTickets = ({
+    fetchData,
+    customerTicket,
+    setCustomerTicket,
+    handleResolved,
+    resolvedTicket,
+}) => {
     const promiseHandled = use(fetchData);
 
-    // remove tickets that are already resolved
+    // Remove tickets that are already resolved
     const visibleTickets = promiseHandled.filter(
         ticket => !resolvedTicket.some(resolved => resolved.id === ticket.id)
-    )
+    );
 
     return (
-        <div className="max-w-7xl mx-auto mb-10 grid grid-cols-11 gap-5 ">
-            <div className=" col-span-8 grid grid-cols-2 gap-5">
-                {
-                    visibleTickets.map(tickets => <Ticket
+        <div className="max-w-7xl mx-auto px-6 lg:px-0 mb-10 grid grid-cols-1 lg:grid-cols-11 gap-6">
+
+            {/* Tickets */}
+            <div className="lg:col-span-8 grid grid-cols-1 md:grid-cols-2 gap-5">
+                {visibleTickets.map(tickets => (
+                    <Ticket
                         key={tickets.id}
                         customerTicket={customerTicket}
                         setCustomerTicket={setCustomerTicket}
                         tickets={tickets}
-                    ></Ticket>)
-                }
+                    />
+                ))}
             </div>
-            <div className=" col-span-3">
-                <TasksSection handleResolved={handleResolved}
+
+            {/* Tasks */}
+            <div className="lg:col-span-3">
+                <TasksSection
+                    handleResolved={handleResolved}
                     customerTicket={customerTicket}
-                ></TasksSection>
+                />
             </div>
 
         </div>
